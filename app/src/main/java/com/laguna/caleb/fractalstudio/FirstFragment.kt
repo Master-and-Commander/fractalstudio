3ac4c182a492697dd71d5cd42bf3f4f5efdadcda
ISENGRIM\caleb <calebdlaguna@hotmail.com>
2020-06-08 17:44:59 -0400

    slow progress

--- app/src/main/java/com/laguna/caleb/fractalstudio/FirstFragment.kt
+++ app/src/main/java/com/laguna/caleb/fractalstudio/FirstFragment.kt
@@ -7,25 +7,56 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+        // sad will need to read up on this stuff again;
+        // link to look at later https://developer.android.com/guide/topics/ui/layout/recyclerview
+
+
+        //viewManager = LinearLayoutManager(this)
+        //viewAdapter = MyAdapter(myDataset)
+         /*
+        recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter
+            adapter = viewAdapter
+
+        }
+
+          */
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        /*
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
+         */
     }
 }
